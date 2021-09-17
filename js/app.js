5a3f96476cfb3cc2710a0bb7d12dfbba03ee2f80
uyiuy27 <ilona_ptak@o2.pl>
2021-09-17 18:54:31 +0200

    poprawki wyglądu menu rozwijalnego dla wyboru języków

--- js/app.js
+++ js/app.js
@@ -1,5 +1,7 @@
 var menu = document.querySelector(".menu-languages-copy");
 var list = document.querySelector(".languages-list");
+// var forPatient = document.querySelector(".options-li");
+// var forPatientList = document.querySelector(".for-patient");
 
 menu.addEventListener("click", function(){
     if(list.style.display === "block") {
@@ -8,3 +10,11 @@ menu.addEventListener("click", function(){
         list.style.display = "block";
     }
 })
+
+// forPatient.addEventListener("click", function(){
+//     if(forPatientList.style.display === "block") {
+//         forPatientList.style.display = "none";
+//     } else {
+//         forPatientList.style.display = "block";
+//     }
+// })
