b86c41a4220f8fafa2b4939d72e53390bbcfa6ea
uyiuy27 <ilona_ptak@o2.pl>
2021-09-17 19:53:07 +0200

    dodanie eventu

--- js/app.js
+++ js/app.js
@@ -1,5 +1,8 @@
 var menu = document.querySelector(".menu-languages-copy");
 var list = document.querySelector(".languages-list");
+
+var touchmove = document.querySelector("tu wpisać to co będzie do obsługiwania zdarzenia");
+const imageList = document.querySelectorAll(".site-example-img");
 // var forPatient = document.querySelector(".options-li");
 // var forPatientList = document.querySelector(".for-patient");
 
@@ -11,6 +14,7 @@ menu.addEventListener("click", function(){
     }
 })
 
+// DLA PACJENTA LISTA ROZWIJANA EVENT
 // forPatient.addEventListener("click", function(){
 //     if(forPatientList.style.display === "block") {
 //         forPatientList.style.display = "none";
@@ -18,3 +22,22 @@ menu.addEventListener("click", function(){
 //         forPatientList.style.display = "block";
 //     }
 // })
+
+
+// EVENT -> przesunięcie obrazka
+// let imageIndex = 0;
+
+// imageList[imageIndex].className = "visible";
+
+// touchmove.addEventListener("touchmove", function (event) {
+//     console.log("przesuwanko");
+//     if (imageIndex < imageList.length - 1) {
+//         imageList[imageIndex].classList.remove("visible");
+//         imageList[imageIndex+1].className = "visible";
+//         imageIndex = imageIndex + 1;
+//     } else {
+//         imageList[imageIndex].classList.remove("visible");
+//         imageList[0].className = "visible";
+//         imageIndex = 0;
+//     }
+// });
