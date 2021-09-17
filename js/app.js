89619c86841600a9ee3b3c61303ac5558b4b9619
uyiuy27 <ilona_ptak@o2.pl>
2021-09-18 00:24:54 +0200

    rozwijane menu bez ikonek z footerem

--- js/app.js
+++ js/app.js
@@ -1,46 +1,56 @@
-var menu = document.querySelector(".menu-languages-copy");
-var list = document.querySelector(".languages-list");
+window.addEventListener("DOMContentLoaded", (event) => {
+    var menu = document.querySelector(".menu-languages-copy");
+    var list = document.querySelector(".languages-list");
 
-var mobileMenu = document.querySelector(".mobile-menu");
-var mobileList = document.querySelector(".mobile-list-menu");
-var clinicalTrials = document.querySelector(".clinical-trials");
-var aboutUs = document.querySelector(".about-us container");
-var location = document.querySelector(".our-locations container");
-var footer = document.querySelector(".footer");
+    var mobileMenu = document.querySelector(".mobile-menu");
+    var mobileList = document.querySelector(".mobile-list-menu");
+    var clinicalTrials = document.querySelector(".clinical-trials");
+    var aboutUs = document.querySelector(".about-us");
+    var location = document.querySelector(".our-locations");
+    var mobileContact = document.querySelector(".mobile-contact-section");
+    var footer = document.querySelector(".footer");
+    var iBar = document.querySelector("#menu-bar");
+    var iTimes = document.querySelector("#menu-times");
 
-var touchmove = document.querySelector("tu wpisać to co będzie do obsługiwania zdarzenia");
-const imageList = document.querySelectorAll(".site-example-img");
+    var touchmove = document.querySelector("tu wpisać to co będzie do obsługiwania zdarzenia");
+    const imageList = document.querySelectorAll(".site-example-img");
 // var forPatient = document.querySelector(".options-li");
 // var forPatientList = document.querySelector(".for-patient");
 
-menu.addEventListener("click", function(){
-    if(list.style.display === "block") {
-        list.style.display = "none";
-    } else {
-        list.style.display = "block";
-    }
-})
+    menu.addEventListener("click", function(){
+        if(list.style.display === "block") {
+            list.style.display = "none";
+        } else {
+            list.style.display = "block";
+        }
+    })
 
-// mobileMenu.addEventListener("touchstart", function(){
-//     if(mobileList.style.display === "block") {
-//         mobileList.style.display = "none";
-//         clinicalTrials.style.display = "block";
-//         aboutUs.style.display = "block";
-//         location.style.display = "block";
-//         footer.style.display = "block";
-        
-//         // var i = document.querySelector(".mobile-menu-button").firstChild;
-//         // i.classList.remve(fa fa-bars fa-2x);
-//         // i.classList.add(fas fa-times fa2x);
-//     } else {
-//         mobileList.style.display = "block";
-//         clinicalTrials.style.display = "none";
-//         aboutUs.style.display = "none";
-//         footer.style.display = "none";
-//         // i.classList.remve(fas fa-times fa-2x);
-//         // i.classList.add(fa fa-bars fa-2x);
-//     }
-// }) 
+    mobileList.style.display = "none";
+
+    mobileMenu.addEventListener("touchstart", function(){
+        if(mobileList.style.display === "block") {
+            mobileList.style.display = "none";
+            clinicalTrials.style.display = "block";
+            aboutUs.style.display = "block";
+            location.style.display = "block";
+            footer.style.display = "block";
+            mobileContact.style.display = "block";
+            // iTimes.style.display = "none";
+            // iBar.style.display = "block";
+
+        } else {
+            mobileList.style.display = "block";
+            clinicalTrials.style.display = "none";
+            aboutUs.style.display = "none";
+            footer.style.display = "none";
+            mobileContact.style.display = "none";
+            location.style.display = "none";
+            // iTimes.style.display = "block";
+            // iBar.style.display = "none";
+
+
+        }
+    })
 
 // DLA PACJENTA LISTA ROZWIJANA EVENT
 // forPatient.addEventListener("click", function(){
@@ -69,3 +79,7 @@ menu.addEventListener("click", function(){
 //         imageIndex = 0;
 //     }
 // });
+});
+
+
+
